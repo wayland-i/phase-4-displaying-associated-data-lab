04d27cfa90a258f7283f75ce97b323042db2be9e
Ian Iversen <ianiversen1@gmail.com>
2022-09-07 08:47:07 -0400

    commit tree d47365c7ba4fdd86a6a5222ce818376ae5d7bdc8

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,9 @@
 class ItemsController < ApplicationController
+
+
+    #GET /items
+    def index
+        items = Item.all
+        render json: items, include: :user
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,10 @@
 class UsersController < ApplicationController
+
+
+
+    #GET /user/:id
+    def show
+        user = User.find(params[:id])
+        render json: user, include: :items
+    end
 end
